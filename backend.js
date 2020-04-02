32b05ba7ad25bdcb3f139c838b109b8219dabf99
ALoserIsMe <32465750+ALoserIsMe@users.noreply.github.com>
2020-04-02 16:07:40 +0100

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,121 @@
+var arr1A2     = ["amitriptyline", "caffeine", "clormipramine", "clozapine", "cyclobenzaprine", "duloxetine", "estradiol", "fluvoxamine", "haloperidol", "imipramine", "mexiletine", "nabumetine", "naproxen", "olanzipine", "ondansetron", "phenacetine", "propanolol", "riluzole", "ropivacaine", "tacrine", "theophylline", "trizanidine", "triameterene", "verapamil", "warfarin", "zileuton", "zolmitriptan"];
+var arr2B6     = ["artemisnin", "bupropion", "cyclophospamide", "efavirenz", "ifosphamide", "ketamine", "meperidine", "methadone", "nevirapine", "propafol", "selegiline", "sorafenib"];
+var arr2C8     = ["amodiaquine", "cerivastatin", "daclitaxel", "repaglinide", "sorafenib", "torsemide"];
+var arr2C9     = ["amitriptyline", "capecitabine", "celecoxib", "clopidogrel", "diclofenac", "doxepin", "fluoxetine", "fluvastatin", "glibenclamide", "glimepiride", "glipizide", "glyburide", "ibuprofen", "irbesartan", "lesinurad", "lornoxicam", "losartan", "meloxicam", "nateglinide", "phenytoin", "piroxicam", "rosiglitazone", "naproxen", "warfarin", "suprofen", "tamoxifen", "tolbutamide", "tolbutamide", "torsemide", "valproic acid", "venlafaxine", "voriconazole", "zakirlukast"];
+var arr2C19    = ["amitriptyline", "atomoxetine", "brivaracetam", "carisoprodol", "chloramphenicol", "citalopram", "clomipramine", "clopidogrel", "cyclophosphamide", "diazepam", "doxepin", "escitalopram", "esomeprazole", "flibanserin", "hexobarbital", "imipramine", "indomethacin", "labetalol", "lansoprazole", "moclobemide", "nelfinavir", "nilutamide", "omeprazole", "pantoprazole", "phenobarbitone", "phenytoin", "primidone", "progesterone", "proguanil", "propanolol", "mephobarbital", "warfarin", "mephenytoin", "suvorexant", "teniposide", "venlafaxine", "voriconazole"] ;
+var arr2D6     = ["alprenolol", "amitriptyline", "amphetamine", "aripiprazole", "atomoxetine", "brexpiprazole", "bufuralol", "cariprazine", "carvedilol", "chlorpheniramine", "chlorpromazine", "citalopram", "clomipramine", "clonidine", "codeine", "debrisoquine", "desipramine", "deutetrabenazine", "dexfenfluramine", "dextromethorphan", "donepezil", "doxepin", "duloxetine", "eliglustat", "encainide", "escitalopram", "flecainide", "fluoxetine", "fluvoxamine", "haloperidol", "ibrutinib", "imipramine", "lidocaine", "methoxyamphetamine", "metoclopramide", "mexiletine", "minaprine", "nebivolol", "netupitant/palonosetron", "nortriptyline", "ondansetron", "oxycodone", "paroxetine", "perhexiline", "perphenazine", "phenacetin", "phenformin", "pimavanserin", "promethazine", "propafenone", "propranolol", "risperidone", "risperidone", "rucaparib", "metoprolol", "sparteine", "tamoxifen", "tetrabenzine", "thioridazine", "timolol", "tramadol", "valbenazine", "venlafaxine", "zuclopenthixol"];
+var arr2E1     = ["acetaminophen", "aniline", "benzene", "chlorzoxazone", "enflurane", "ethanol", "halothane", "isoflurane", "methoxyflurane", "dimethylformamide", "sevoflurane", "theophylline"];
+var arr3A457   = ["abemaciclib", "acalabrutinib", "alectinib", "alfentanil", "alprazolam", "amitriptyline", "amlodipine", "aprepitant", "aripiprazole", "astemizole", "atorvastatin", "boceprevir", "brexpiprazole", "brigatinib", "buspirone", "cafergot", "caffeine", "carbamazepine", "cariprazine", "cerivastatin", "chlorpheniramine", "cilostazol", "cisapride", "citalopram", "clarithromycin", "clopidogrel", "cobimetinib", "cocaine", "codeine", "copanlisib", "cyclosporine", "daclatasvir", "dapsone", "deflazacort", "dexamethasone", "dextromethorphan", "diazepam", "diltiazem", "docetaxel", "domperidone", "doxepin", "efavirenz", "elbasvir/grazoprevir", "eliglustat", "eplerenone", "erythromycin", "escitalopram", "esomeprazole", "estradiol", "felodipine", "fentanyl", "finasteride", "flibanserin", "gleevec", "haloperidol", "hydrocortisone", "ibrutinib", "idelalisib", "indinavir", "irinotecan", "isavuconazonium sulfate", "ivabradine", "laam", "lansoprazole", "lenvatinib", "lercanidipine", "lidocaine", "lovastatin", "methadone", "midazolam", "naldemedine", "naloxegol", "nateglinide", "nelfinavir", "neratinib", "netupitant/palonosetron", "nevirapine", "nifedipine", "nisoldipine", "nitrendipine", "azithromycin", "pravastatin", "rosuvastatin", "olaparib", "omeprazole", "ondansetron", "osimertinib", "palbociclib", "panobinostat", "pantoprazole", "pimavanserin", "pimozide", "progesterone", "propranolol", "quetiapine", "quinidine", "quinine", "regorafenib", "ribociclib", "risperidone", "ritonavir", "rolapitant", "romidepsin", "salmeterol", "saquinavir", "selexipag", "sildenafil", "simvastatin", "sirolimus", "sonidegib", "sorafenib", "sunitinib", "suvorexant", "tacrolimus", "tamoxifen", "taxol", "telaprevir", "telithromycin", "terfenadine", "testosterone", "torisel", "tramadol", "trazodone", "valbenazine", "velpatasvir", "vemurafenib", "venetoclax", "venlafaxine", "verapamil", "vincristine", "voriconazole", "zaleplon", "ziprasidone", "zolpidem"];
+var colNameArr = ["1A2", "2B6", "2C8", "2C9", "2C19", "2D6", "2E1", "3A457"];
+var colArr     = [arr1A2, arr2B6, arr2C8, arr2C9, arr2C19, arr2D6, arr2E1, arr3A457];
+
+function outputColumns(paramArr){
+	var toFind      = paramArr[0];
+	var itemColumns = paramArr[1];
+	var outputList  = [];
+	
+	for (i = 0; i < itemColumns.length; i++){
+		if (itemColumns[i] == "1A2"){
+			for (x = 0; x < arr1A2.length; x++){
+				outputList.push(arr1A2[x]);
+			}
+		}
+		if (itemColumns[i] == "2B6"){
+			for (x = 0; x < arr2B6.length; x++){
+				outputList.push(arr2B6[x]);
+			}
+		}
+		if (itemColumns[i] == "2C8"){
+			for (x = 0; x < arr2C8.length; x++){
+				outputList.push(arr2C8[x]);
+			}
+		}
+		if (itemColumns[i] == "2C9"){
+			for (x = 0; x < arr2C9.length; x++){
+				outputList.push(arr2C9[x]);
+			}
+		}
+		if (itemColumns[i] == "2C19"){
+			for (x = 0; x < arr2C19.length; x++){
+				outputList.push(arr2C19[x]);
+			}
+		}
+		if (itemColumns[i] == "2D6"){
+			for (x = 0; x < arr2D6.length; x++){
+				outputList.push(arr2D6[x]);
+			}
+		}
+		if (itemColumns[i] == "2E1"){
+			for (x = 0; x < arr2E1.length; x++){
+				outputList.push(arr2E1[x]);
+			}
+		}
+		if (itemColumns[i] == "3A457"){
+			for (x = 0; x < arr3A457.length; x++){
+				outputList.push(arr3A457[x]);
+			}
+		}
+	}
+	if (outputList.length == 0){
+		alert("That medicine can't be found. Please ensure it is spelled correctly");
+	} else {
+		outputList.sort();
+		for (x = 0; x < outputList.length; x++){
+			try{
+				if (outputList[x] == outputList[x+1]){
+					var index = outputList.indexOf(outputList[x+1]);
+					console.log(outputList[x+1] + " removed");
+					outputList.splice(index, 1);
+				}
+				else if (outputList[x] == outputList[x-1]) {
+					var index = outputList.indexOf(outputList[x-1]);
+					console.log(outputList[x-1] + " removed");
+					outputList.splice(index, 1);
+				}
+			} 																														//TODO: Ensure this doesn't remove wrong medicines
+			catch(err){
+				break;
+			}
+		}
+		
+		return outputList
+	}
+}
+
+function columnSearch(toFind){
+	var itemColumns = [];
+	var toOutput    = [];
+	
+	for (x = 0; x < colArr.length; x++) {
+		for (y = 0; y < colArr[x].length; y++){
+			if (colArr[x][y] == toFind) {
+				itemColumns.push(colNameArr[x]);
+			}
+		}
+	}
+	
+	toOutput.push(toFind);
+	toOutput.push(itemColumns);
+	return toOutput;
+}
+
+function validateInput(toValidate){
+	var allowedChars = "abcdefghijklmnopqrstuvwxyz/ ";
+	var inputIsValid = true;
+	
+	//var usersInput = document.getElementById("medicineName").value;
+	
+	for (x = 0; x < toValidate.length; x++){
+		if (allowedChars.indexOf(toValidate[x]) == -1){
+			inputIsValid = false;
+			break;
+		}
+	}
+	
+	if (inputIsValid == true){
+		console.log("Valid input");
+		return true;
+	} else {
+		return false;
+	}
+}
